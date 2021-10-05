cf2b6788108c2dc1e5503c8c4c45880bf74ff73e
Darkenstein.R <58870562+Ayobama@users.noreply.github.com>
2021-10-05 10:09:03 +0100

    following my ideal

--- mytest.sh
+++ mytest.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
+a #!/bin/bash
 #
-# Script Name: mytest.sh
+# Script Name: createnewshfile.sh
 #
-# Author: Name of creator
-# Date : Date of creation
+# Author: Ayobami
+# Date : 5th of Oct
 #
-# Description: The following script reads in a text file called /path/to/file 
-#              and creates a new file called /path/to/newfile
+# Description: The following script creates an empty new schell script called Newfile.sh
 #
-# Run Information: This script is run automatically every Monday of every week at 20:00hrs from
-#                  a crontab entry. 
+# Run Information: This script is manually run after an absolute invocation
 #
 # Error Log: Any errors or output associated with the script can be found in /path/to/logfile
 #
 
-exec 1>>/home/john/scripts/output.log
-exec 2>>/home/john/scripts/errors.log
+exec 1>>Bash-Shell-Scripting/output.log
+exec 2>>Bash-Shell-Scripting/errors.log
 
 test=$(date) 
 echo "Today's date is $test"
@@ -24,3 +22,4 @@ echo "I'll create a new file and then edit"
 touch Newfile.sh
 
 cat ./Newfile.sh
+#failed I haven't figured it yet

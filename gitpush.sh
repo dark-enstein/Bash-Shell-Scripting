6072fd7553a964906102e8a41ce63f85b59b045b
Darkenstein.R <58870562+Ayobama@users.noreply.github.com>
2021-10-12 07:11:21 +0100

    WIP: Day 6 II)[D[D[D(

--- gitpush.sh
+++ gitpush.sh
@@ -1,15 +1,20 @@
-#! /bin/bash
+
+git status
 
 git add .
+
+git status
 
 echo 'Enter the commit message:'
 read commitMessage
 
 git commit -m "$commitMessage"
 
+git status
+
 #echo 'Enter the name of the branch:'
 #read branch
 
-git push
+git push #origin $branch
 
 read

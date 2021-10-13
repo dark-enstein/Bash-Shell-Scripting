e0eacbd5d5b5d44bdf49b8ee1b810cc6aee208f9
Darkenstein.R <58870562+Ayobama@users.noreply.github.com>
2021-10-13 07:20:22 +0100

    WIP: new commit

---
+++ Bash-project/Readme.md
@@ -0,0 +1,7 @@
+Project: Exploring Webscraping on Bash in a docker container
+
+Also: Installed packages, and expored Vim
+
+Here I made my first iteration and followed the tutorial
+
+For more information on my actual project, check within the ./Project1.dir folder
---
+++ copy-from-container.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#Author:Ayobami Bamigboye
+#Description: A simple script to copy the contents from a specific folder in the container to a predefined path in the system
+#Title: copy-from-container.sh
+#Run Information: I should only use it while I still have the container specified below running.
+
+container=61c31a32504a
+
+echo "Copying from ./Bash-project folder to '/c/Users/User/OneDrive - STEM Institute of Learning LTD GTE/Documents/Cloud Computing/DevOps/Bash Shell Scripting' in container " $container
+
+echo 'Starting'
+
+docker cp 61c31a32504a:/Bash-project '/c/Users/User/OneDrive - STEM Institute of Learning LTD GTE/Documents/Cloud Computing/DevOps/Bash Shell Scripting'
+
+echo 'Ended'
+
+echo 'Operation completed'
+
+echo 'Warning: This script have been programmed to be used only the container and the respective filepaths specified above. If you intend to edit the script to make it more robust edit this script by running this command: $nano copy-from-container.sh'
